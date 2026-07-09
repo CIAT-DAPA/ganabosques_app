15ebe91cf193e44d595c348f8cdc689423d0ff0d
Victor Hernandez <victor.993_@hotmail.com>
2026-07-09 11:28:46 -0500

    Docker Implementation

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,14 @@
 // API configuration
 //export const API_URL = "http://localhost:8000/";
-export const API_URL = "https://ganaapi.alliance.cgiar.org/";
+
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/";
+
+export const KEYCLOAK_URL =
+  process.env.NEXT_PUBLIC_KEYCLOAK_URL || "";
+
+export const KEYCLOAK_REALM =
+  process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "";
+
+export const KEYCLOAK_CLIENT_ID =
+  process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID || "";
